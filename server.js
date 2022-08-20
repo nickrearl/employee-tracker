b8db3911e6565ae02dfcefa04a47f1c9e503a506
Nick Earl <nick.r.earl@gmail.com>
2022-08-20 02:21:10 -0400

    display options

--- server.js
+++ server.js
@@ -1,5 +1,47 @@
 const db = require('./db/connection')
 const inquirer = require('inquirer')
+const cTable = require('console.table')
+
+const displayDepartments = () => {
+  const sql = `SELECT departments.name AS Department FROM departments`
+
+  db.query(sql, (err, rows) => {
+    const table = cTable.getTable(rows)
+    console.log(table)
+  })
+}
+
+const displayRoles = () => {
+  const sql = `SELECT roles.name AS Role,
+                departments.name AS Department
+                FROM roles
+                LEFT JOIN departments
+                ON roles.departments_id = departments.id`
+
+  db.query(sql, (err, rows) => {
+    const table = cTable.getTable(rows)
+    console.log(table)
+  })
+}
+
+const displayEmployees = () => {
+  const sql = `SELECT e.first_name AS 'First Name',
+              e.last_name AS 'Last Name',
+              departments.name AS Department,
+              roles.name AS Role,
+              roles.salary AS Salary,
+              CONCAT(m.first_name,' ',m.last_name) AS Manager
+              FROM employees e
+              LEFT JOIN employees m ON e.manager_id = m.id
+              INNER JOIN roles ON e.roles_id = roles.id
+              INNER JOIN departments ON roles.departments_id = departments.id
+              `
+
+  db.query(sql, (err, rows) => {
+    const table = cTable.getTable(rows)
+    console.log(table)
+  })
+}
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -17,6 +59,32 @@ const promptUser = () => {
       ]
     }
   ])
+  .then(runOption => {
+    if (runOption.option === 'view all departments') {
+      console.log('Showing all departments')
+      console.log('===============================')
+      displayDepartments()
+    }
+    else if (runOption.option === 'view all roles') {
+      console.log('Showing all roles')
+      console.log('===============================')
+      displayRoles()
+    }
+    else if (runOption.option === 'view all employees') {
+      console.log('Showing all employees')
+      console.log('===============================')
+      displayEmployees()
+    }
+    else if (runOption.option === 'add a department') {
+      console.log('add a department')
+    }
+    else if (runOption.option === 'add a role') {
+      console.log('add a role')
+    }
+    else if (runOption.option === 'add an employee') {
+      console.log('add an employee')
+    }
+  })
 }
 
 promptUser()
